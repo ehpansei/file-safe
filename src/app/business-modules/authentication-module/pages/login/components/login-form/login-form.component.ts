d99e673c939d95543c1c1230c8b92d0bb18ab627
DESKTOP-PHUEU2L\PC Multimedia <p.castro275@gmail.com>
2022-02-22 14:46:38 +0000

    validate email format with regex, small fix on login form component

--- src/app/business-modules/authentication-module/pages/login/components/login-form/login-form.component.ts
+++ src/app/business-modules/authentication-module/pages/login/components/login-form/login-form.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/forms';
 import { AuthenticationService } from '@infrastructure-module/services/authentication/authentication.service';
 import { finalize } from 'rxjs';
+import { AppConfig } from 'src/configs/app.config';
 
 @Component({
   selector: 'login-form',
@@ -45,7 +46,10 @@ export class LoginFormComponent implements OnInit {
 
   private initFormGroup(): void {
     this.formGroup = this.fb.group({
-      email: this.fb.control(null, [Validators.required]),
+      email: this.fb.control(null, [
+        Validators.required,
+        Validators.pattern(AppConfig.emails.regexFormat)
+      ]),
       password: this.fb.control(null, [Validators.required])
     });
   }
--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -12,4 +12,8 @@ export class AppConfig {
   static readonly lists = {
     filePageSize: 16
   };
+
+  static readonly emails = {
+    regexFormat: '[A-Za-z0-9._%-+]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}'
+  };
 }
